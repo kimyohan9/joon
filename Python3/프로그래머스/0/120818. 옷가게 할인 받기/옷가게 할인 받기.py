dce126dc49f405b1eee4daf68b732d820e92d1df
kimyohan9 <kdygks1996@gmail.com>
2024-12-24 09:54:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,12 +1,22 @@
 # def solution(price):
+#     answer = price
 #     if price >= 500000:
 #         answer = price * 0.8  # 20% 할인
 #     elif price >= 300000:
-#         answer =price * 0.9  # 10% 할인
+#         answer = price * 0.9  # 10% 할인
 #     elif price >= 100000:
 #         answer = price * 0.95  # 5% 할인
-#     return int(answer)  
+#     return int(answer) 
 
+# def solution(price):
+#     if price >= 500000:
+#         return int(price * 0.8)  # 20% 할인
+#     elif price >= 300000:
+#         return int(price * 0.9)  # 10% 할인
+#     elif price >= 100000:
+#         return int(price * 0.95)  # 5% 할인
+#     else:
+#         return price  # 할인 없음
 
 # def solution(price):
 #     if price >= 500000:
@@ -19,12 +29,11 @@
 #         return price
 
 def solution(price):
-    if price >= 500000:
-        answer = price * 0.8  # 20% 할인
-    elif price >= 300000:
-        answer = price * 0.9  # 10% 할인
-    elif price >= 100000:
-        answer = price * 0.95  # 5% 할인
-    else:
-        answer = price  # 할인 없음
-    return int(answer)
+    if price>=500000:
+        price = price *0.8
+    elif price>=300000:
+        price = price *0.9
+    elif price>=100000:
+        price = price * 0.95
+    return int(price)
+
