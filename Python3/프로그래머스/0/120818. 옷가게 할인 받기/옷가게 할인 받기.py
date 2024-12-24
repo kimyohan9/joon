58f641a0a55f860c00bed0fb23baf17eb393c1c5
kimyohan9 <kdygks1996@gmail.com>
2024-12-24 09:48:04 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,24 +1,14 @@
-def solution(price):
-    if price >= 500000:
-        return int(price * 0.8)  # 20% 할인
-    elif price >= 300000:
-        return int(price * 0.9)  # 10% 할인
-    elif price >= 100000:
-        return int(price * 0.95)  # 5% 할인
-    else:
-        return price  # 할인 없음
-
 # def solution(price):
 #     if price >= 500000:
-#         return int(price * 0.8)  # 20% 할인
+#         answer = price * 0.8  # 20% 할인
 #     elif price >= 300000:
-#         return int(price * 0.9)  # 10% 할인
+#         answer =price * 0.9  # 10% 할인
 #     elif price >= 100000:
-#         return int(price * 0.95  # 5% 할인
-#     else:
-#         return price  # 할인 없음
+#         answer = price * 0.95  # 5% 할인
+#     return int(answer)  
 
-    # def solution(price):
+
+# def solution(price):
 #     if price >= 500000:
 #         return price - (price * 0.2)
 #     elif price >= 300000:
@@ -27,3 +17,14 @@ def solution(price):
 #         return price - (price * 0.05)
 #     else:
 #         return price
+
+def solution(price):
+    if price >= 500000:
+        answer = price * 0.8  # 20% 할인
+    elif price >= 300000:
+        answer = price * 0.9  # 10% 할인
+    elif price >= 100000:
+        answer = price * 0.95  # 5% 할인
+    else:
+        answer = price  # 할인 없음
+    return int(answer)
